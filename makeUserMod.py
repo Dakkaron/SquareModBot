a5aa13760e956b6598c34e744bd45a77c9cc7504
Dakkaron <dak1st@gmx.at>
2023-07-23 00:17:07 +0200

    Fixed makeUserMod.py

--- makeUserMod.py
+++ makeUserMod.py
@@ -5,7 +5,7 @@ from pythorhead import Lemmy
 
 print("This script gives a target bot moderator rights on a target community,")
 print("without the bot having to post there first.")
-if input("Are you sure you want to do that? (y/N)").strip() not in ["y","Y"]:
+if input("Are you sure you want to do that? (y/N) ").strip() not in ["y","Y"]:
 	print("Aborting")
 	exit()
 instanceUrl = input("Instance URL:   ")
@@ -21,14 +21,14 @@ print(f"Your password:   {currentPass}")
 print(f"Bot user name:   {botName}")
 print(f"Community name:  {community}")
 
-if input("Is everything correct? (y/N)").strip() not in ["y","Y"]:
+if input("Is everything correct? (y/N) ").strip() not in ["y","Y"]:
 	print("Aborting")
 	exit()
 
 lemmy = Lemmy(instanceUrl)
-lemmy.login(currentUser, currentPassword)
+lemmy.log_in(currentUser, currentPass)
 
 communityId = lemmy.community.get(name = community)["community_view"]["community"]["id"]
 personId = lemmy.user.get(username = botName)["person_view"]["person"]["id"]
 
-lemmy.community.add_mod_to_community(added = True, communityId = communityId, personId = personId)
+lemmy.community.add_mod_to_community(added = True, community_id = communityId, person_id = personId)
